8dc5e30080da73157dc922440acfaddd761611e2
Benjaplusx <64096748+Benjaplusx@users.noreply.github.com>
2020-10-13 15:25:27 +0200

    Delete Unifi.sh

--- Unifi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget https://dl.ui.com/unifi/6.0.23/unifi_sysvinit_all.deb
-sudo apt-get install openjdk-8-jre-headless -y
-sudo apt-get install haveged -y
-sudo dpkg -i unifi_sysvinit_all.deb
