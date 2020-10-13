9878548d293198cc160a3d1d26bc6843a8bec943
Benjaplusx <64096748+Benjaplusx@users.noreply.github.com>
2020-10-13 15:07:50 +0200

    Update Unifi.sh

--- Unifi.sh
+++ Unifi.sh
@@ -1,6 +1,6 @@
-echo "Unifi 6.0.23"
+#!/bin/bash
+
 wget https://dl.ui.com/unifi/6.0.23/unifi_sysvinit_all.deb
 sudo apt-get install openjdk-8-jre-headless -y
 sudo apt-get install haveged -y
 sudo dpkg -i unifi_sysvinit_all.deb
-echo "Done"
