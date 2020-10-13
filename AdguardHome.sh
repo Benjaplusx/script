f3186e35484fee7646b24328e7212330320e0b43
Benjaplusx <64096748+Benjaplusx@users.noreply.github.com>
2020-10-13 19:21:44 +0200

    Update AdguardHome.sh

--- AdguardHome.sh
+++ AdguardHome.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 wget https://static.adguard.com/adguardhome/release/AdGuardHome_linux_arm.tar.gz
+sudo tar xvf AdGuardHome_linux_arm.tar.gz
 cd AdGuardHome
 sudo ./AdGuardHome -s install
